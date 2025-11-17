512450c06baee5dabc5662d6733967fd2c801d52
Aleksandra Mazur <ola.mazur2005@gmail.com>
2025-11-17 10:59:12 +0100

    nowe

---
+++ 03-ControlStructures/4.4.py
@@ -0,0 +1,8 @@
+university = 'Krakow University of Economics'
+university_expanded = 'K r a k o w U n i v e r s i t y o f E c o n o m i c s'
+
+for char in university:
+    university_expanded = university_expanded + ... + ...
+
+print(university) # original university name
+print(university_expanded) # expanded university name
--- test.py
+++ test.py
@@ -20,3 +20,29 @@ else:
     
     print('end')
 
+for x in range(5):
+    print(x+1,"nie będę przeszkadzała")
+
+    for x in range(1,6):
+    print(x,"nie będę przeszkadzała")
+
+for x in range(5,11):
+    print("nie będę przeszkadzała")
+
+for x in range(5,11,2):
+    print("nie będę przeszkadzała")
+
+
+    name = "Aleksandra"
+    for char in name:
+        print(char,end="\n lub ;")
+
+    for char in "Ania"
+
+while warunek:
+    instrukcje
+
+x = 1
+while x < 5:
+    print("Hello")
+    x = x + 1 <---mega wazne!!!
