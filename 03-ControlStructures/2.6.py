5fa4275226dffa650799489f575f2e419ebb1844
Aleksandra Mazur <ola.mazur2005@gmail.com>
2025-12-14 19:18:35 +0100

    zadanka

---
+++ 03-ControlStructures/2.6.py
@@ -0,0 +1,8 @@
+number = int(input('Enter your number: '))
+
+if number > 0:
+    print(f'{number} is positive')
+elif number == 0:
+    print(f'{number} is zero')
+elif number < 0:
+    print(f'{number} is negative')
