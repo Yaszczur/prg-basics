887168ae1cc1e9853ec02054e49b68c67ceaea0b
Aleksandra Mazur <ola.mazur2005@gmail.com>
2026-01-12 11:14:31 +0100

    4zadanie

---
+++ 13-Test3/p4.py
@@ -0,0 +1,11 @@
+def f(fnc, prods):
+    return ",".join([fnc(p) for p in prods])
+
+if __name__ == "__main__":
+    prods = ["water", "cheese", "tomato"]
+    
+    fnc1 = lambda x: "id:" + x[:2]
+    print(f(fnc1, prods))
+    
+    fnc2 = lambda x: (x[0] + x[-1]).upper()
+    print(f(fnc2, prods))
